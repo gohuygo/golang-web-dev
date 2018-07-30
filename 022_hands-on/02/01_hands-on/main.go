5147f908f2c439a23bc5c65153e0fd36d34dde9a
Huy <huy@gohuygo.com>
2018-07-30 15:55:37 -0700

    multiple views

--- 022_hands-on/02/01_hands-on/main.go
+++ 022_hands-on/02/01_hands-on/main.go
@@ -1,8 +1,10 @@
 // # Building upon the code from the previous problem:
 
-// Change your RESPONSE HEADER "content-type" from "text/plain" to "text/html"
+// Add code to respond to the following METHODS & ROUTES:
+//   GET /
+//   GET /apply
+//   POST /apply
 
-// Change the RESPONSE from "CHECK OUT THE RESPONSE BODY PAYLOAD" (and everything else it contained: request method, request URI) to an HTML PAGE that prints "HOLY COW THIS IS LOW LEVEL" in <h1> tags.
 package main
 
 import (
@@ -51,27 +53,110 @@ func serve(conn net.Conn) {
       fmt.Println("THIS IS THE END OF THE HTTP REQUEST HEADERS")
       break
     }
+
     i++
   }
- 
+
+  switch {
+  case method == "GET" && uri == "/":
+    handleIndex(conn)
+  case method == "GET" && uri == "/apply":
+    handleApply(conn)
+  case method == "POST" && uri == "/apply":
+    handleApplyPost(conn)
+  default:
+    handleDefault(conn)
+  }
+
+}
+
+func handleIndex(c net.Conn) {
   body := `
     <!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
-      <title>TEST</title>
+      <title>GET INDEX</title>
     </head>
     <body>
-      <h1>"HOLY COW THIS IS LOW LEVEL"</h1>
+      <h1>"GET INDEX"</h1>
+      <a href="/">index</a><br>
+      <a href="/apply">apply</a><br>
     </body>
     </html>
   `
-  io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
-  fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-  fmt.Fprint(conn, "Content-Type: text/html\r\n")
-  io.WriteString(conn, "\r\n")
-  io.WriteString(conn, body)
+  io.WriteString(c, "HTTP/1.1 200 OK\r\n")
+  fmt.Fprintf(c, "Content-Length: %d\r\n", len(body))
+  fmt.Fprint(c, "Content-Type: text/html\r\n")
+  io.WriteString(c, "\r\n")
+  io.WriteString(c, body)
+}
 
+func handleApply(c net.Conn) {
+  body := `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>GET DOG</title>
+    </head>
+    <body>
+      <h1>"GET APPLY"</h1>
+      <a href="/">index</a><br>
+      <a href="/apply">apply</a><br>
+      <form action="/apply" method="POST">
+      <input type="hidden" value="In my good death">
+      <input type="submit" value="submit">
+      </form>
+    </body>
+    </html>
+  `
+  io.WriteString(c, "HTTP/1.1 200 OK\r\n")
+  fmt.Fprintf(c, "Content-Length: %d\r\n", len(body))
+  fmt.Fprint(c, "Content-Type: text/html\r\n")
+  io.WriteString(c, "\r\n")
+  io.WriteString(c, body)
+}
 
+func handleApplyPost(c net.Conn) {
+  body := `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>POST APPLY</title>
+    </head>
+    <body>
+      <h1>"POST APPLY"</h1>
+      <a href="/">index</a><br>
+      <a href="/apply">apply</a><br>
+    </body>
+  </html>
+  `
+  io.WriteString(c, "HTTP/1.1 200 OK\r\n")
+  fmt.Fprintf(c, "Content-Length: %d\r\n", len(body))
+  fmt.Fprint(c, "Content-Type: text/html\r\n")
+  io.WriteString(c, "\r\n")
+  io.WriteString(c, body)
+}
+
+func handleDefault(c net.Conn) {
+  body := `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>default</title>
+    </head>
+    <body>
+      <h1>"default"</h1>
+    </body>
+    </html>
+  `
+  io.WriteString(c, "HTTP/1.1 200 OK\r\n")
+  fmt.Fprintf(c, "Content-Length: %d\r\n", len(body))
+  fmt.Fprint(c, "Content-Type: text/html\r\n")
+  io.WriteString(c, "\r\n")
+  io.WriteString(c, body)
 }
 
